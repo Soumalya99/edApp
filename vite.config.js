f5a67e20e2bb2fa59ab0d86c2ad6116d4308fa86
Soumalya Chakraborty <soumalyachakraborty00@gmail.com>
2025-08-16 20:52:45 +0530

    vite config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                home: 'index.html',
+                batch: 'batch.html',
+                selection: 'selection.html',
+                teams: 'team.html',
+                resources: 'resources.html'
+            }
+        }
+    },
+});
